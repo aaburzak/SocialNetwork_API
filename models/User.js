0f22a548fe3ee58239ab8edf3e1042c3e2686ed4
Aaron Burzak <aaron.j.burzak@gmail.com>
2022-04-30 09:11:42 -0400

    built out models folder

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema (
+    {
+        username: {
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            // **** EMAIL AUTHENTICATOR STILL REQUIRED****
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+        }
+    ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length
+});
+
+const User = model ('User', userSchema);
+
+module.exports = User
