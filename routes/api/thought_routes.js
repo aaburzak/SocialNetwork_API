74b0a12b14c927177b4ac90fb68ebdedfde1f646
Aaron Burzak <aaron.j.burzak@gmail.com>
2022-05-02 13:06:20 -0400

    built out routes/api/thought_routes.js

---
+++ routes/api/thought_routes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const {
+    getAllThought,
+    getThoughtById,
+    addThought,
+    addReaction,
+    updateThought,
+    removeThought,
+    removeReaction
+} = require('../../controllers/thought_control');
+
+router
+    .route('/')
+    .get(getAllThought)
+    .post(addThought);
+
+router
+    .route('/:thoughtId')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(removeThought);
+
+router
+    .route('/:thoughtId/reactions')
+    .post(addReaction);
+
+router
+    .route('/:thoughtId/reactions/:reactionId')
+    .delete(removeReaction)
+
+    module.exports = router;
