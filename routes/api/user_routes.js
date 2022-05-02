ef0c21c89c60777d5fb2dfdabba221a35d7e221a
Aaron Burzak <aaron.j.burzak@gmail.com>
2022-05-02 15:02:55 -0400

    built out route/api/user_routes.js

---
+++ routes/api/user_routes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const{
+    getAllUser,
+    getUserById,
+    addUser,
+    addFriend,
+    updateUser,
+    removeUser,
+    removeFriend   
+} = require('../../controllers/user_control');
+
+router
+    .route('/')
+    .get(getAllUser)
+    .post(addUser);
+
+router
+    .route('/:userId')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(removeUser);
+    
+router
+    .route('/:userId/friends/:friendId')
+    .post(addFriend)
+    .delete(removeFriend)
+
